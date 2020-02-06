196911cdf263198d459ccf3a174eb4a3cd57b4bc
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-06 04:32:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(arr,name){
+  return arr.filter((el)=> {return el.toLowerCase().indexOf(name.toLowerCase()) == 0 });
+}
+function fuzzyMatch(arr,name){
+  return arr.filter((el)=> {return el.toLowerCase().indexOf(name.toLowerCase()) == 0 });
+}
+
+function matchName(arr,name){
+  return arr.filter((el)=> {return el.name.toLowerCase().indexOf(name.toLowerCase()) == 0 });
+}
