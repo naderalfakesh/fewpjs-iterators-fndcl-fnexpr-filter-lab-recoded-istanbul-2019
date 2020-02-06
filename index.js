ee4de87dccbf44321923b38a16271990ac1e79b2
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-06 04:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution here
 function findMatching(arr,name){
   const rule1=  name.toLowerCase()+"*";
-  return arr.filter((el)=> {return el.toLowerCase() === rule1});
+  return arr.filter((el)=> {return el.toLowerCase() === name.toLowerCase()});
 }
