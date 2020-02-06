ccd9a90ebe64afc062f7c65f51c5425d1fd21915
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-06 04:30:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution here
 function findMatching(arr,name){
-  const rule1=  name.toLowerCase()+"*";
   return arr.filter((el)=> {return el.toLowerCase().indexOf(name.toLowerCase()) == 0 });
 }
